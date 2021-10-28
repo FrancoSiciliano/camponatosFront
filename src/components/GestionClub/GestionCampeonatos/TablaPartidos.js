7da026e25fb779425952919eea146b22520e51f5
agussturba <49353314+agussturba@users.noreply.github.com>
2021-10-27 21:45:34 -0300

    Create TablaPartidos.js

---
+++ src/components/GestionClub/GestionCampeonatos/TablaPartidos.js
@@ -0,0 +1,52 @@
+import { Table,Button } from "react-bootstrap"
+import { useLocation } from 'react-router-dom'
+import {useEffect, useState} from "react";
+import axios from "axios";
+export const TablaPartidos=()=>{
+  const location = useLocation()
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+        const response = await axios(`http://localhost:8080/getAllPartidos`);
+        const newData = response.data;
+        setData(newData);
+    };
+    fetchData();
+});
+  if(data){
+    return(
+    <Table striped bordered hover>
+        <thead>
+            <tr><th colSpan="6">Nombre Campeonato</th></tr>
+          <tr>
+            <th>Fecha</th>
+            <th>NroZona</th>
+            <th>Categoria</th>
+            <th>ClubLocal</th>
+            <th>ClubVisitante</th>
+            <th colSpan="2">
+        <form  classname ="searchBar" onsubmit="event.preventDefault();" role="search">
+        <input classname="searchBox"
+        id="search" type="search" placeholder="Filtrar por Nombre" autofocus required />
+        <button type="button" classname="botonsearch">search</button>
+</form>
+</th>
+    </tr>
+  </thead>
+  <tbody>
+  {data.map((partido,index)=>{
+  return(
+    <tr key={index}>
+      <td>{partido.nroFecha}</td>
+      <td>{partido.nroZona}</td>
+      <td>{partido.categoria}</td>
+      <td>{partido.clubLocal.nombre}</td>
+      <td>{partido.clubVisitante.nombre}</td>
+      <td><Button classname="botonesTablas" type="submit" class="btn btn-primary btn-sm">Validar</Button></td>
+    </tr>)
+  })}
+</tbody>
+</Table>)}
+else{
+  return (<h1>Cargando...</h1>)
+    }}
