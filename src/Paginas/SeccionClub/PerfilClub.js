385de2444f861796705243ea1567cef8c53a84f3
agussturba <49353314+agussturba@users.noreply.github.com>
2021-10-27 21:44:53 -0300

    Create PerfilClub.js

---
+++ src/Paginas/SeccionClub/PerfilClub.js
@@ -0,0 +1,10 @@
+import { DatosClub } from "../../components/DatosClub/DatosClub"
+import NavBarClub from "../../components/NavBars/NavBarClub"
+
+export const PerfilClub= ()=>{
+    return(
+        <div><NavBarClub/>
+            <DatosClub/>
+            </div>
+    )
+}
