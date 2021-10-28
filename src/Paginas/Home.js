17165552305d2e9ac8ba5fbd415c8e44b19dd2d3
agussturba <49353314+agussturba@users.noreply.github.com>
2021-10-27 21:44:55 -0300

    Delete Home.js

--- src/Paginas/Home.js
+++
@@ -1,9 +0,0 @@
-import NavBarGeneral from "../components/NavBarGeneral/NavBarGeneal"
-const lista1 = ["hola","adios","mañana"]
-export const Home = () =>{
-    return(<div className="contenedorHome">
-    <NavBarGeneral enlaces={lista1} />
-        <h1>Bienvenidos a Campeonatos</h1>
-    </div>
-    )
-}
