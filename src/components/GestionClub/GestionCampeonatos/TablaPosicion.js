8cd494732b350fcc7e2faeaa45f87b7f4eee6558
agussturba <49353314+agussturba@users.noreply.github.com>
2021-10-28 20:12:57 -0300

    Update TablaPosicion.js

--- src/components/GestionClub/GestionCampeonatos/TablaPosicion.js
+++ src/components/GestionClub/GestionCampeonatos/TablaPosicion.js
@@ -1,34 +1,57 @@
-import { render } from "@testing-library/react"
-import { Table } from "react-bootstrap"
-export const TablaPosicion=()=>{
-    render()
-    return <Table striped bordered hover>
+import { Table,Button } from "react-bootstrap"
+import { useLocation } from 'react-router-dom'
+import {useEffect, useState} from "react";
+import { axios } from "axios";
+export const TablaPosicion=(props)=>{
+  let location = useLocation()
+  console.log(location)
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+        const response = await axios(`http://localhost:8080/getAllPartidosByCampeonato?idCampeonato=`+location.state);
+        const newData = response.data;
+        setData(newData);
+    };
+    fetchData();
+});
+  if(data){
+    return(
+    <Table striped bordered hover>
         <thead>
+            <tr><th colSpan="6">Nombre Campeonato</th></tr>
           <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table> 
-}
+            <th>Club</th>
+            <th>G</th>
+            <th>E</th>
+            <th>P</th>
+            <th>GA</th>
+            <th>GE</th>
+            <th>Dif</th>
+            <th>Puntos</th>
+            <th>Prom</th>
+            <th colSpan="2">
+        <form  classname ="searchBar" onsubmit="event.preventDefault();" role="search">
+        <input classname="searchBox"
+        id="search" type="search" placeholder="Filtrar por Nombre" autofocus required />
+        <button type="button" classname="botonsearch">search</button>
+</form>
+</th>
+    </tr>
+  </thead>
+  <tbody>
+  {data.map((tabla,index)=>{
+
+    <tr key={index}>
+      <td>{tabla.nroFecha}</td>
+      <td>{tabla.nroZona}</td>
+      <td>{tabla.categoria}</td>
+      <td>{tabla.clubLocal.nombre}</td>
+      <td>{tabla.clubVisitante.nombre}</td>
+      <td><Button classname="botonesTablas" type="submit" class="btn btn-primary btn-sm">Validar</Button></td>
+    </tr>
+  })}
+</tbody>
+</Table>)}
+else{
+  return (<h1>Cargando...</h1>)
+    }}
