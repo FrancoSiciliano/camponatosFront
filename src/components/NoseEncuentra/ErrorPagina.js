2dcfbdd611b884fc879b4f31d0d42bbefe8d67fa
agussturba <49353314+agussturba@users.noreply.github.com>
2021-11-01 12:22:43 -0300

    Create ErrorPagina.js

---
+++ src/components/NoseEncuentra/ErrorPagina.js
@@ -0,0 +1,9 @@
+import Estadio from "../../assets/images/Estadio.jpg";
+export const ErrorPagina = (props) =>{
+    return(
+        <div style={{backgroundImage:`url(${Estadio})`,width:"1280px",
+        height: "900px",backgroundRepeat:"no-repeat"}}>
+
+        </div>
+    )
+}
