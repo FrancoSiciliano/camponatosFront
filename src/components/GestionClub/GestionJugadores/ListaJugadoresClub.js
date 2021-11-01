0eeb6d80af700c91a8df144ee67ee3c89f19837e
agussturba <49353314+agussturba@users.noreply.github.com>
2021-11-01 12:22:53 -0300

    Update ListaJugadoresClub.js

--- src/components/GestionClub/GestionJugadores/ListaJugadoresClub.js
+++ src/components/GestionClub/GestionJugadores/ListaJugadoresClub.js
@@ -1,8 +1,7 @@
 import { Table,Button } from "react-bootstrap";
-import '../GestionCampeonatos/Listado.css'
+import '../GestionJugadores/ListaJugadoresClub.css'
 import {useEffect, useState} from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import NavBarClub from "../../NavBars/NavBarClub";
 
 
@@ -17,12 +16,12 @@ export const ListaJugadoresClub = (props) =>{
   if(data){
       return( <div>
               <NavBarClub/>
-              <div className="Tabla">
+              <div className="TablaJugadores">
               <Table striped bordered hover sm >
                 <thead>
                     <tr borderless>
-                        <th  colSpan="6">
-                          Jugadores del CLub
+                        <th  colSpan="15">
+                          Jugadores del Club
                         </th>
                       </tr>
                   <tr>
@@ -63,8 +62,8 @@ export const ListaJugadoresClub = (props) =>{
                   <td>{jugadores.fechaNacimiento}</td>
                   <td>{jugadores.fechaAlta}</td>
                   <td>{jugadores.estado}</td>
-                  <td><Button classname="botonesTablas" type="submit" class="btn btn-primary btn-sm"><Link to='/TablaPosicion'> Modificar</Link></Button></td>
-                  <td><Button classname="botonesTablas" type="submit" class="btn btn-primary btn-sm"><Link to='/TablaPartidos'> Eliminar</Link></Button></td>
+                  <td><Button classname="botonesTablas" type="submit" class="btn btn-primary btn-sm">Modificar</Button></td>
+                  <td><Button classname="botonesTablas" type="submit" class="btn btn-primary btn-sm"> Eliminar</Button></td>
                 </tr>)
               })}
             </tbody>
