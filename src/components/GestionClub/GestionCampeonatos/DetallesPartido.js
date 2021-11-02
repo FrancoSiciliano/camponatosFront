b1eed71bcf308758f58be35ff2e4bfe558dac5ab
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-11-02 19:32:20 -0300

    DetallesPartido en progreso

--- src/components/GestionClub/GestionCampeonatos/DetallesPartido.js
+++ src/components/GestionClub/GestionCampeonatos/DetallesPartido.js
@@ -1,40 +1,79 @@
-import { Table,Button } from "react-bootstrap"
-import { useLocation } from 'react-router-dom'
+import {Table, Button} from "react-bootstrap"
+import {useLocation} from 'react-router-dom'
 import {useEffect, useState} from "react";
 import axios from "axios";
-export const DetallesPartido=()=>{
-  let location = useLocation()
-  console.log(location)
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-        const response = await axios(`http://localhost:8080/encontrarPartido?idPartido=2`);
-        const newData = response.data;
-        setData(newData);
-    };
-    fetchData();
-});
-console.log(data)
-  if(data){
-    data.map((partido,index)=>{
-    return(
-    <form className="Detalles">
-    <label className="Detalles-Label">NroFecha:{partido.nroFecha}</label>
-    <label className="Detalles-Label">NroZona:{partido.nroZona}</label>
-    <label className="Detalles-Label">Categoria:{partido.categoria}</label>
-    <label className="Detalles-Label">Incidentes:{partido.incidentes}</label>
-    <label className="Detalles-Label">Club Local:{partido.clubLocal.nombre}</label>
-    <label className="Detalles-Label">Club Visitante:{partido.clubVisitante.nombre}</label>
-    <label className="Detalles-Label">Goles Locales:{partido.golesLocal}</label>
-    <label className="Detalles-Label">Goles Visitantes:{partido.golesVisitante}</label>
-    <label className="Detalles-Label">Campeonato:{partido.campeonato.descripcion}</label>
-    <label className="Detalles-Label">Jugadores Locales:{partido.nroFecha}</label>
-    <label className="Detalles-Label">Jugadores Visitantes:{partido.nroFecha}</label>
-    <label className="Detalles-Label">Jugadores Locales:{partido.nroFecha}</label>
-    <label className="Detalles-Label">Faltas:{partido.nroFecha} </label>
-    <label className="Detalles-Label">Goles:{partido.nroFecha} </label>
-    </form>
-    )})}
-else{
-  return (<h1>No se crearon Partidos para este campeonato</h1>)
-    }}
+import './DetallesPartido.css'
+import './Listado.css'
+
+export const DetallesPartido = (props) => {
+    let location = useLocation();
+
+    const [datosPartido, setDatosPartido] = useState(null);
+    const [faltasLocal, setFaltasLocal] = useState(null);
+    const [faltasVisitante, setFaltasVisitante] = useState(null);
+
+    useEffect(async () => {
+        const fetchDataPartido = async () => {
+            const response = await axios(`http://localhost:8080/encontrarPartido?idPartido=1`);
+            const newData = response.data;
+            setDatosPartido(newData);
+
+        };
+
+        const fetchDataFaltas = async () => {
+            const responseFaltasL = await axios.get(`http://localhost:8080/getFaltasByClubAndPartido?idClub=1&idPartido=1`)
+            const dataFaltasL = responseFaltasL.data;
+
+            const responseFaltasV = await axios.get(`http://localhost:8080/getFaltasByClubAndPartido?idClub=2&idPartido=1`)
+            const dataFaltasV = responseFaltasV.data;
+
+            setFaltasLocal(dataFaltasL);
+            setFaltasVisitante(dataFaltasV);
+            console.log(dataFaltasL);
+
+        }
+
+        await fetchDataPartido();
+        await fetchDataFaltas();
+    }, []);
+
+    if (datosPartido && faltasLocal && faltasVisitante) {
+        return (<>
+            <div className="titulo-partido">
+                <h1>{datosPartido.clubLocal.nombre}</h1>
+                <h1>VS.</h1>
+                <h1>{datosPartido.clubVisitante.nombre}</h1>
+            </div>
+            <div className="comparacion-partido">
+                <Table striped bordered hover sm className="local">
+                    <tbody>
+                    <tr>
+                        <td>Goles: {datosPartido.golesLocal ? datosPartido.golesLocal : "0"}</td>
+                    </tr>
+                    <tr>
+                        <td>Validado: {datosPartido.convalidaLocal ? "Si" : "No"}</td>
+                    </tr>
+                    <tr>
+                        <td>Cantidad Faltas: {faltasLocal.length}</td>
+                    </tr>
+                    </tbody>
+                </Table>
+                <Table striped bordered hover sm className="visitante">
+                    <tbody>
+                    <tr>
+                        <td>Goles: {datosPartido.golesVisitante ? datosPartido.golesVisitante : "0"}</td>
+                    </tr>
+                    <tr>
+                        <td>Validado: {datosPartido.convalidaVisitante ? "Si" : "No"}</td>
+                    </tr>
+                    <tr>
+                        <td>Cantidad Faltas: {faltasVisitante.length}</td>
+                    </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </>);
+    } else {
+        return (<h1>Nada q mostrar</h1>)
+    }
+}
