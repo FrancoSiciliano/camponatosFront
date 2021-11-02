4931fee0cb2cb9f1ce75ed8bedc9da9349d918a9
agussturba <49353314+agussturba@users.noreply.github.com>
2021-11-02 16:49:50 -0300

    Create NavBarAdministracion.js

---
+++ src/components/NavBars/NavBarAdministracion.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container'
+import './NavBarCampeonatos.css'
+import { Navbar,Nav,NavDropdown } from 'react-bootstrap'
+function NavBarAdministracion (props) {
+  return(
+    <Navbar bg="dark" classname="Navbarcamp">
+    <Container>
+      <Navbar.Brand href="/homeClub">Administracion</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/perfilClub">Perfil</Nav.Link>
+          <NavDropdown title="Gestionar" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/gestionar/campeonato">Campeonatos</NavDropdown.Item>
+            
+            <NavDropdown.Item href="/gestionar/jugadores"> Jugadores</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="/">Exit</Nav.Link>
+          
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>)}
+
+export default NavBarAdministracion;
