ef83ad8e75f04290c10668a9eecf8ca1104a0f0a
Juli <julian_gamietea@hotmail.com>
2021-11-12 16:37:40 -0300

    arreglada nav admin

--- src/components/NavBars/NavBarAdministracion.js
+++ src/components/NavBars/NavBarAdministracion.js
@@ -6,17 +6,9 @@ import {MdSportsSoccer} from 'react-icons/all'
 import { useState } from 'react'
 import { BiLogOut} from 'react-icons/bi';
 import { BsPencil } from 'react-icons/bs'
-import { PopUpEdicion } from '../PopUp/PopUpEdicion'
 
 function NavBarAdministracion () {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const [tipo, setTipo] = useState("");
-  const handleClick = (tipo) => {
-    console.log(tipo);
-    setTipo(tipo);
-    setShow(true);
-  }
+  
   return(
       <Navbar>
       <Container fluid>
@@ -25,22 +17,14 @@ function NavBarAdministracion () {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto" >
-            <div className = 'editar-admin cerrarsesion'>
-              <BsPencil style={{position:'relative', top:'5px' ,width:'25px', height:'25px', color:'white'}}/>
-              <NavDropdown title="EDITAR" id="basic-nav-dropdown" classname="LinkNavbar">
-                <NavDropdown.Item onClick={()=> handleClick('JUGADOR')} > PERFIL JUGADOR </NavDropdown.Item>
-                <NavDropdown.Item onClick={()=> handleClick('CLUB')} > PERFIL CLUB </NavDropdown.Item>
-                <NavDropdown.Item onClick={()=> handleClick('REPRESENTANTE')} > PERFIL REPRESENTANTE </NavDropdown.Item>
-              </NavDropdown>
-            </div>
-            <div className = 'cerrarsesion'>
+            <div className = 'cerrarsesionadmin'>
               <BiLogOut  style={{position:'relative', top:'5px' ,width:'30px', height:'30px', color:'white'}}/>
               <Nav.Link href="/">CERRAR SESION</Nav.Link>
             </div>
             </Nav>
           </Navbar.Collapse>
           
-          <PopUpEdicion handleClose={handleClose} show={show} tipo={tipo ? tipo : "a"}/> 
+         
           
           
       </Container>
