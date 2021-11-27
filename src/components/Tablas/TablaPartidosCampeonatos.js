cceb03a51efe2ee4f295f682666274b256d6c787
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-11-27 14:33:26 -0300

    se controla que los datos del partido hayan sido cargados antes de poder verlos y que no aparezca lista de jugadores si el responsable no pertenece al equipo

--- src/components/Tablas/TablaPartidosCampeonatos.js
+++ src/components/Tablas/TablaPartidosCampeonatos.js
@@ -1,5 +1,5 @@
-import {Table, Form} from "react-bootstrap"
-import {useLocation, Link} from 'react-router-dom'
+import {Table, Form, Button} from "react-bootstrap"
+import {useLocation, Link, useHistory} from 'react-router-dom'
 import {useEffect, useState} from "react";
 import axios from "axios";
 import './TablaPartidosCampeonatos.css'
@@ -8,15 +8,23 @@ import NavBarResponsable from "../NavBars/NavBarResponsable";
 import {PantallaCarga} from "../PantallaCarga/PantallaCarga";
 
 export const TablaPartidosCampeonatos = () => {
-    let location = useLocation()
+    const history = useHistory();
     const [data, setData] = useState(null);
     const rol = localStorage.getItem("rol");
+    const [clubResponsable, setClubResponsable] = useState(null);
+    const [isPartidoSeleccionadoCargado, setIsPartidoSeleccionadoCargado] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await axios(`http://localhost:8080/getPartidosByCampeonato?idCampeonato=${location.state.idCampeonato}`);
+            const response = await axios(`http://localhost:8080/getPartidosByCampeonato?idCampeonato=${history.location.state.idCampeonato}`);
             const newData = response.data;
             setData(newData);
+
+            if (rol === "RESPONSABLE") {
+                const res = await axios(`http://localhost:8080/getResponsableById?idResponsable=${localStorage.getItem("id")}`);
+                const responsable = res.data;
+                setClubResponsable(responsable.club.idClub);
+            }
         };
 
         fetchData();
@@ -30,6 +38,29 @@ export const TablaPartidosCampeonatos = () => {
         }
     }
 
+    const isPartidoCargado = async (idPartido) => {
+        const res = await axios.get(`http://localhost:8080/encontrarPartido?idPartido=${idPartido}`);
+        const partido = res.data;
+        return partido.golesLocal !== null && partido.golesVisitante !== null;
+    }
+
+    const handleClick = async (idPartido, datosPartido) => {
+        if (await isPartidoCargado(idPartido)) {
+            alert("No puede editar la lista de jugadores en un partido cuyos datos ya fueron cargados")
+        } else {
+            history.push('/tabla/partidos/lista_jugadores', datosPartido)
+
+        }
+    }
+
+    const handleClickDetalles = async (idPartido, datosPartido) => {
+        if (await isPartidoCargado(idPartido)) {
+            history.push('/detalles/partidos', datosPartido)
+        } else {
+            alert("Los resultados del partido aún no fueron cargados por el administrador");
+        }
+    }
+
     if (data) {
         return (<div>
             {navbar()}
@@ -37,7 +68,7 @@ export const TablaPartidosCampeonatos = () => {
                 <Table striped bordered hover>
                     <thead>
                     <tr>
-                        <th colSpan="8">{location.state.descrip}</th>
+                        <th colSpan="8">{history.location.state.descrip}</th>
                     </tr>
                     <tr>
                         <th>Fecha</th>
@@ -53,7 +84,7 @@ export const TablaPartidosCampeonatos = () => {
                     </tr>
                     </thead>
                     <tbody>
-                    {data.map((partido, index) => {
+                    {data.map( (partido, index) => {
                         var ids = partido.idPartido
                         var categ = partido.categoria
                         return (
@@ -63,10 +94,17 @@ export const TablaPartidosCampeonatos = () => {
                                 <td>{categ}</td>
                                 <td>{partido.clubLocal.nombre}</td>
                                 <td>{partido.clubVisitante.nombre}</td>
-                                <td><Link class="btn btn-primary btn-sm" to={{
-                                    pathname: '/tabla/partidos/lista_jugadores',
-                                    state: {idPartido: ids, categoria: categ, campeonato: location.state.idCampeonato, nombrePartido: `${partido.clubLocal.nombre} - ${partido.clubVisitante.nombre} | Fecha número: ${partido.nroFecha} | Fecha: ${partido.fechaPartido}`}
-                                }}> Lista Jugadores</Link></td>
+                                <td><Button class="btn btn-primary btn-sm" onClick={() => handleClickDetalles(partido.idPartido, {
+                                    idPartido: partido.idPartido,
+                                    clubLocal: partido.clubLocal,
+                                    clubVisitante: partido.clubVisitante,
+                                    clubResponsable: clubResponsable
+                                })}>Detalles</Button></td>
+                                {rol === "RESPONSABLE" && (partido.clubLocal.idClub === clubResponsable || partido.clubVisitante.idClub === clubResponsable) &&
+                                <td><Button onClick={() => handleClick(partido.idPartido, {idPartido: ids,
+                                    categoria: categ,
+                                    campeonato: history.location.state.idCampeonato,
+                                    nombrePartido: `${partido.clubLocal.nombre} - ${partido.clubVisitante.nombre} | Fecha número: ${partido.nroFecha} | Fecha: ${partido.fechaPartido}`})} class="btn btn-primary btn-sm" > Lista Jugadores</Button></td>}
                             </tr>)
                     })}
                     </tbody>
