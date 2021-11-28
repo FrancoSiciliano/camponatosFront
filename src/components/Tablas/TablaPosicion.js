6aa6e7d9816ed721814aa1257c6d4cfec1663c8d
Juli <julian_gamietea@hotmail.com>
2021-11-28 14:47:41 -0300

    tabla posiciones resuelto

--- src/components/Tablas/TablaPosicion.js
+++ src/components/Tablas/TablaPosicion.js
@@ -9,17 +9,26 @@ import { Spinner } from "react-bootstrap";
 export const TablaPosicion = (props) => {
     const history = useHistory();
     const [tablaPosiciones, setTablaPosiciones] = useState(null);
-
+    const [campeonato, setCampeonato] = useState(null);
     useEffect(() => {
         const fetchData = async () => {
+            const camp = await axios(`http://localhost:8080/encontrarCampeonato?idCampeonato=${history.location.state.campeonato}`)
+            const newCamp = camp.data;
+            setCampeonato(newCamp);
+            if(newCamp.tipoCampeonato == 'Puntos'){
+                const response = await axios(`http://localhost:8080/getTablaPosicionesByCampeonato?idCampeonato=${history.location.state.campeonato}`);
+                const newData = response.data;
+                setTablaPosiciones(newData);
 
-            const response = await axios(`http://localhost:8080/getTablaPosicionesByCampeonato?idCampeonato=${history.location.state.campeonato}`);
-
-            const newData = response.data;
-            setTablaPosiciones(newData);
+            }else{
+                const response = await axios(`http://localhost:8080/getTablaPosicionesByZona?idCampeonato=${history.location.state.campeonato}`);
+                const newData = response.data;
+                setTablaPosiciones(newData);
+            }
+        
         };
         fetchData();
-    });
+    },[]);
 
     const navbar = () => {
         if (history.location.state.tipo === "RESPONSABLES") {
@@ -32,46 +41,99 @@ export const TablaPosicion = (props) => {
     }
 
     if (tablaPosiciones) {
-        return (
-            <div>
-                {navbar()}
-                <Table striped bordered hover>
-                    <thead>
-                    <tr>
-                        <th colSpan="12">{tablaPosiciones[0].campeonato.descripcion}</th>
-                    </tr>
-                    <tr>
-                        <th>Club</th>
-                        <th>J</th>
-                        <th>G</th>
-                        <th>E</th>
-                        <th>P</th>
-                        <th>GA</th>
-                        <th>GE</th>
-                        <th>Dif</th>
-                        <th>Puntos</th>
-                        <th>Prom</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {tablaPosiciones.map((tabla, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{tabla.id.nombre}</td>
-                                <td>{tabla.cantidadJugados}</td>
-                                <td>{tabla.cantidadGanados}</td>
-                                <td>{tabla.cantidadEmpatados}</td>
-                                <td>{tabla.cantidadPerdidos}</td>
-                                <td>{tabla.golesFavor}</td>
-                                <td>{tabla.golesContra}</td>
-                                <td>{tabla.diferenciaGoles}</td>
-                                <td>{tabla.puntos}</td>
-                                <td>{tabla.promedio}</td>
-                            </tr>
-                        )
-                    })}
-                    </tbody>
-                </Table></div>)
+        if(campeonato.tipoCampeonato == 'Puntos' ){
+            return (
+                <div>
+                    {navbar()}
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th colSpan="12">{campeonato.descripcion}</th>
+                        </tr>
+                        <tr>
+                            <th>Club</th>
+                            <th>J</th>
+                            <th>G</th>
+                            <th>E</th>
+                            <th>P</th>
+                            <th>GA</th>
+                            <th>GE</th>
+                            <th>Dif</th>
+                            <th>Puntos</th>
+                            <th>Prom</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {tablaPosiciones.map((tabla, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{tabla.id.nombre}</td>
+                                    <td>{tabla.cantidadJugados}</td>
+                                    <td>{tabla.cantidadGanados}</td>
+                                    <td>{tabla.cantidadEmpatados}</td>
+                                    <td>{tabla.cantidadPerdidos}</td>
+                                    <td>{tabla.golesFavor}</td>
+                                    <td>{tabla.golesContra}</td>
+                                    <td>{tabla.diferenciaGoles}</td>
+                                    <td>{tabla.puntos}</td>
+                                    <td>{tabla.promedio}</td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </Table></div>)
+        }else{
+            return (
+                <div>
+                    {navbar()}
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th colSpan="12">{campeonato.descripcion}</th>
+                        </tr>
+                        <tr>
+                            <th>Club</th>
+                            <th>J</th>
+                            <th>G</th>
+                            <th>E</th>
+                            <th>P</th>
+                            <th>GA</th>
+                            <th>GE</th>
+                            <th>Dif</th>
+                            <th>Puntos</th>
+                            <th>Zona</th>
+                            <th>Prom</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                        tablaPosiciones.map((tabla, index) => {
+                            return (
+                                tabla.map((elemento) => {
+                                    return(
+                                    <tr key={index}>
+                                        <td>{elemento.id.nombre}</td>
+                                        <td>{elemento.cantidadJugados}</td>
+                                        <td>{elemento.cantidadGanados}</td>
+                                        <td>{elemento.cantidadEmpatados}</td>
+                                        <td>{elemento.cantidadPerdidos}</td>
+                                        <td>{elemento.golesFavor}</td>
+                                        <td>{elemento.golesContra}</td>
+                                        <td>{elemento.diferenciaGoles}</td>
+                                        <td>{elemento.puntos}</td>
+                                        <td>{index+1}</td>
+                                        <td>{elemento.promedio}</td>
+                                    </tr>
+                                    )
+                                })
+                                
+                            )
+                        })
+                        }
+                        </tbody>
+                    </Table></div>)
+        }
+        
     } else {
         return (<div>
             {navbar()}
